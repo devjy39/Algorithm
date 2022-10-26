f84db1c26d6ed9c74fec3790a1357c3ecf10008c
devjy39 <devjy0309@gmail.com>
2022-10-26 17:42:10 +0900

    가장 긴 증가하는 부분 수열 - dp, LIS(Longest Increasing SubSequence) binary search 풀이법

--- src/problem/baekjoon/dp/No11053.java
+++ src/problem/baekjoon/dp/No11053.java
@@ -3,33 +3,49 @@ package problem.baekjoon.dp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class No11053 {
-    static int MAX_LEN = 1001;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<>(n);
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int[] dp = new int[MAX_LEN]; // index로 끝나는 수에서 최대 개수 저장
-
-        for (int i = 0; i < n; i++) {
-            int num = Integer.parseInt(st.nextToken());
-
-            int max = 0;
-            for (int j = 1; j < num; j++) {
-                if(dp[j]>0)
-                    max = Math.max(max, dp[j]);
-            }
-            dp[num] = max + 1;
-        }
         br.close();
 
-        int answer = 0;
-        for (int i = 1; i < MAX_LEN; i++) {
-            answer = Math.max(answer, dp[i]);
+        list.add(Integer.parseInt(st.nextToken()));
+        for (int i = 1; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+
+            if (list.get(list.size() - 1) < num) {
+                list.add(num);
+            } else {
+                list.set(binSearch(list, num), num);
+            }
         }
-        System.out.println(answer);
+
+        System.out.println(list.size());
+    }
+
+    private static int binSearch(List<Integer> list, int num) {
+        int low = 0;
+        int high = list.size() - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >> 1;
+
+            if (num < list.get(mid)) {
+                high = mid - 1;
+            } else if (num > list.get(mid)) {
+                low = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return low;
     }
 }
