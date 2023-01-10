d0b55660b969dcf38c427195fc5848d7a5e38cca
devjy39 <devjy0309@gmail.com>
2023-01-11 03:14:10 +0900

    오아시스 재결합 - stack
    스택으로 더 간단하게 처리한 풀이
    쌍을 찾는 문제이므로 쌍을 찾는데 집중.
    O(n)
    https://www.acmicpc.net/problem/3015

--- src/problem/baekjoon/linear/No3015.java
+++ src/problem/baekjoon/linear/No3015.java
@@ -6,12 +6,12 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class No3015 {
-    static class NumberCount {
-        int number;
+    static class Pair {
+        int height;
         int count;
 
-        public NumberCount(int number) {
-            this.number = number;
+        public Pair(int height) {
+            this.height = height;
             this.count = 1;
         }
     }
@@ -19,44 +19,35 @@ public class No3015 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        Stack<Integer> stack = new Stack<>();
-        long result = 0;
-        int max = 0;
+        Stack<Pair> stack = new Stack<>();
+        long pairs = 0;
 
         for (int i = 0; i < n; i++) {
-            int number = Integer.parseInt(br.readLine());
-            if (max < number) {
-                result += getPopStackResult(stack, number);
-                max = number;
+            int height = Integer.parseInt(br.readLine());
+            while (!stack.isEmpty() && stack.peek().height < height) {
+                pairs += getSum(stack.pop().count); // 같은 수일 경우 1 ~ 개수 합
             }
-            stack.push(number);
+
+            if (!stack.isEmpty() && stack.peek().height == height) {
+                stack.peek().count++; // 같은 수 묶기 (최적화)
+            } else {
+                stack.push(new Pair(height));
+            }
+
+            if (stack.size() > 1) { // 왼쪽에 더 큰 수가 존재하면 height와 쌍
+                pairs++;
+            }
         }
 
-        System.out.println(result + getPopStackResult(stack, stack.pop()));
+        while (!stack.isEmpty()) { // 단조감소만 남은 스택
+            pairs += getSum(stack.pop().count - 1);
+        }
+
+        System.out.println(pairs);
     }
 
-    private static long getPopStackResult(Stack<Integer> outStack, int right) {
-        long result = 0;
-        Stack<NumberCount> inStack = new Stack<>();
-        inStack.push(new NumberCount(right));
-
-        while (!outStack.isEmpty()) { // 모든 수가 right 보다 작으니 다 뺀다.
-            int left = outStack.pop(); // left 가 왼쪽 수 일 때 모든 페어 계산
-            while (!inStack.isEmpty() && left > inStack.peek().number) {
-                result += inStack.pop().count;
-            }
-
-            if (!inStack.isEmpty() && left == inStack.peek().number) {  // 같은 수가 있다면
-                result += inStack.peek().count++ +(inStack.size() > 1 ? 1 : 0); // size check -> 맨 끝 수는 커도 포함이기 때문, 같은 수가 많을 수도 있기 때문에 객체로 카운트
-            } else {
-                if (!inStack.isEmpty()) {
-                    result++;
-                }
-                inStack.push(new NumberCount(left)); // inStack 은 단조감소하는 형태가 된다.
-            }
-        }
-
-        return result;
+    private static long getSum(int count) {
+        return (long) count * (count + 1) >> 1;
     }
 
 }
