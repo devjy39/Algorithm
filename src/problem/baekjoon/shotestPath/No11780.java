afb558b036e51c25d3f7b1018edce976d03c9925
devjy39 <devjy0309@gmail.com>
2023-01-22 23:21:42 +0900

    플로이드 2 - 플로이드워셜
    최선의 경로를 업데이트할 때 start -> via -> end, 특정 어느 중간 경유지(via) 하나만 저장 되어있으면 dfs로 모든 경로를 찾아낼 수 있다.
    
    t.c : O(n^3)
    https://www.acmicpc.net/problem/11780

--- src/problem/baekjoon/shotestPath/No11780.java
+++ src/problem/baekjoon/shotestPath/No11780.java
@@ -6,22 +6,14 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class No11780 {
-    static class Path {
-        int node;
-        Path prev;
-
-        public Path(int node) {
-            this.node = node;
-        }
-    }
-
     static class Node {
         int w;
-        Path path;
+        int via;
+        int count;
 
-        public Node(int w, Path path) {
+        public Node(int w) {
             this.w = w;
-            this.path = path;
+            this.count = 2;
         }
 
     }
@@ -29,8 +21,6 @@ public class No11780 {
     static int MAX = 1_000_000_000;
 
     public static void main(String[] args) throws IOException {
-        //System.setIn(new FileInputStream("src/data.txt"));
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
@@ -38,7 +28,7 @@ public class No11780 {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
                 if (i != j && graph[i][j] == null) {
-                    graph[i][j] = new Node(MAX, null);
+                    graph[i][j] = new Node(MAX);
                 }
             }
         }
@@ -50,7 +40,6 @@ public class No11780 {
             int e = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
             graph[s][e].w = Math.min(graph[s][e].w, w);
-            graph[s][e].path = new Path(s);
         }
 
         floyd(n, graph);
@@ -60,7 +49,6 @@ public class No11780 {
 
     private static void floyd(int n, Node[][] graph) {
         for (int via = 1; via <= n; via++) {
-
             for (int i = 1; i <= n; i++) {
                 for (int j = 1; j <= n; j++) {
                     if (i == j || i == via || via == j) {
@@ -69,16 +57,8 @@ public class No11780 {
 
                     if (graph[i][via].w + graph[via][j].w < graph[i][j].w) {
                         graph[i][j].w = graph[i][via].w + graph[via][j].w;
-                        Path prev = graph[via][j].path;
-                        Path cur = graph[i][j].path = new Path(prev.node);
-                        prev = prev.prev;
-
-                        while (prev != null) {
-                            cur.prev = new Path(prev.node);
-                            cur = cur.prev;
-                            prev = prev.prev;
-                        }
-                        cur.prev = graph[i][via].path;
+                        graph[i][j].count = graph[i][via].count + graph[via][j].count - 1;
+                        graph[i][j].via = via;
                     }
                 }
             }
@@ -100,31 +80,30 @@ public class No11780 {
             result.append("\n");
         }
 
-        int[] tmp = new int[101];
-        int count;
-
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                if (i == j || graph[i][j].path == null) {
+                if (i == j || graph[i][j].w == MAX) {
                     result.append(0);
                 } else {
-                    count = 0;
-                    tmp[count] = j;
-                    Path path = graph[i][j].path;
-                    while (path != null) {
-                        tmp[++count] = path.node;
-                        path = path.prev;
-                    }
-
-                    result.append(count + 1).append(" ");
-                    for (int k = count; k >= 0; k--) {
-                        result.append(tmp[k]).append(" ");
-                    }
+                    result.append(graph[i][j].count).append(" ");
+                    findPath(result, graph, i, j);
+                    result.append(j).append(" ");
                 }
                 result.append("\n");
             }
         }
         return result.toString();
+    }
+
+    private static void findPath(StringBuilder result, Node[][] graph, int start, int end) {
+        int via = graph[start][end].via;
+        if (via == 0) {
+            result.append(start).append(" ");
+            return;
+        }
+
+        findPath(result, graph, start, via);
+        findPath(result, graph, via, end);
     }
 
 }
