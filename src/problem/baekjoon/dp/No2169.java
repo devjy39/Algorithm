8a1ff20442e01eb39d361615f7b56e19753e4dba
devjy39 <devjy0309@gmail.com>
2023-02-13 22:13:47 +0900

    로봇 조종하기 - dp
    풀이 업그레이드 1648ms -> 628ms
    
    이전의 풀이가 좌 우 에서 오는 것을 탐색하는데 중복 연산이 있었다. 생각해보니 왼쪽에서 오는 경우의 최선은 dp로 오른쪽으로 가면서 이미 저장되어있는데 이걸 활용하지 않고 있었다. 그래서 왼쪽에서 오는 경우, 오른쪽에서 오는 경우를 나눠서 기존의 N^2 연산 대신 2N으로 줄였다.
    
    시간복잡도는 이제 N * 2M 일 것
    t.c : O(N * M)
    https://www.acmicpc.net/problem/2169

--- src/problem/baekjoon/dp/No2169.java
+++ src/problem/baekjoon/dp/No2169.java
@@ -1,7 +1,6 @@
 package problem.baekjoon.dp;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
@@ -9,60 +8,47 @@ import java.util.StringTokenizer;
 public class No2169 {
 
     public static void main(String[] args) throws IOException {
-        System.setIn(new FileInputStream("src/data.txt"));
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[][] map = new int[N][M];
-
-        for (int i = 0; i < N; i++) {
+        int[][] map = new int[N + 1][M + 1];
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
+            for (int j = 1; j <= M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int[][] dp = new int[N][M];
-        dp[0][0] = map[0][0];
-        for (int i = 1; i < M; i++) {
-            dp[0][i] = dp[0][i - 1] + map[0][i];
+        System.out.println(getMaxValue(N, M, map));
+    }
+
+    private static int getMaxValue(int N, int M, int[][] map) {
+        int[][] dp = new int[N + 1][M + 1];
+        int[] left = new int[M + 1];
+        int[] right = new int[M + 2];
+        left[0] = -100_000;
+        right[M + 1] = -100_000;
+        for (int i = 1; i <= M; i++) {
+            left[i] = dp[1][i] = map[1][i] + dp[1][i - 1];
         }
 
-        for (int i = 1; i < N - 1; i++) {
-            for (int j = 0; j < M; j++) {
-                dp[i][j] = dp[i - 1][j]; // 위에서 내려옴
+        for (int i = 2; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                left[j] = Math.max(dp[i - 1][j], left[j - 1]) + map[i][j]; // 위 or 왼쪽에서 오는 경우
+            }
 
-                int sum = 0;
-                for (int k = j - 1; k >= 0; k--) { //왼쪽에서 옴
-                    sum += map[i][k];
-                    dp[i][j] = Math.max(dp[i][j], sum + dp[i - 1][k]);
-                }
-
-                sum = 0;
-                for (int k = j + 1; k < M; k++) { // 오른쪽에서 옴
-                    sum += map[i][k];
-                    dp[i][j] = Math.max(dp[i][j], sum + dp[i - 1][k]);
-                }
-
-                dp[i][j] += map[i][j]; //대소비교 후 마지막에 더하기
+            if (i == N) {
+                break; // 마지막은 오른쪽에서 못 옴
+            }
+            for (int j = M; j > 0; j--) { // 오른쪽에서 오는 경우
+                right[j] = Math.max(dp[i - 1][j], right[j + 1]) + map[i][j];
+                dp[i][j] = Math.max(left[j], right[j]);
             }
         }
 
-        // 맨 밑 줄
-        if (N == 1) {
-            System.out.println(dp[N - 1][M - 1]);
-            return;
-        }
-
-        dp[N - 1][0] = dp[N - 2][0] + map[N - 1][0];
-
-        for (int i = 1; i < M; i++) {
-            dp[N - 1][i] += Math.max(dp[N - 1][i - 1], dp[N - 2][i]) + map[N - 1][i];
-        }
-        System.out.println(dp[N - 1][M - 1]);
+        return left[M];
     }
 
 }
