d532f58454e1d9ffa11f41cf36d5f54048263adf
devjy39 <devjy0309@gmail.com>
2023-02-25 22:55:04 +0900

    카드 구매하기 - dp
    1개부터 N개 까지 순회하면서 i개의 카드를 구매하는 최대금액을 저장해두고 현재 개수의 카드금액을 계산할때 사용한다.
    1개의 카드를 구매하는 방법은 1가지밖에 없으므로 첫 값은 그냥 얻을 수 있다.
    
    N은 최대 1000 이고 경우의 수는 N^2이 된다.
    t.c : O(N^2)
    https://www.acmicpc.net/problem/11052

---
+++ src/problem/baekjoon/dp/No11052.java
@@ -0,0 +1,31 @@
+package problem.baekjoon.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No11052 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            dp[i] = arr[i];
+            for (int j = 1; j < i; j++) {
+                dp[i] = Math.max(dp[i], dp[j] + dp[i - j]);
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+
+}
