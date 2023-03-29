384e77b8f030d86b407149bffe0d7b474b62cb9f
devjy39 <devjy0309@gmail.com>
2023-03-29 23:26:22 +0900

    가장 긴 감소하는 부분 수열 - dp
    LIS(가장 긴 증가하는 부분 수열) 문제에서 비교연산을 역으로 적용하여 풀이.
    
    t.c : O(nlogn)
    https://www.acmicpc.net/problem/11722

---
+++ src/problem/baekjoon/dp/No11722.java
@@ -0,0 +1,51 @@
+package problem.baekjoon.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class No11722 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<>(n);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        br.close();
+
+        list.add(Integer.parseInt(st.nextToken()));
+        for (int i = 1; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+
+            if (list.get(list.size() - 1) > num) {
+                list.add(num);
+            } else {
+                list.set(binSearch(list, num), num);
+            }
+        }
+
+        System.out.println(list.size());
+    }
+
+    private static int binSearch(List<Integer> list, int num) {
+        int low = 0;
+        int high = list.size() - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >> 1;
+
+            if (num > list.get(mid)) {
+                high = mid - 1;
+            } else if (num < list.get(mid)) {
+                low = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return low;
+    }
+}
