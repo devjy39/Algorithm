bd5f7a9962fbf561a2d3cf82d6478c091f0de4dc
devjy39 <devjy0309@gmail.com>
2023-06-16 23:20:23 +0900

    안테나 - sort
    안테나로부터 모든 집까지의 거리 합이 최소가 되려면 집 위치의 중앙값에 안테나를 설치해야한다.
    고로 정렬 후 중앙값을 출력한다.
    
    t.c = O(nlogn)
    https://www.acmicpc.net/problem/18310

---
+++ src/problem/baekjoon/math/No18310.java
@@ -0,0 +1,26 @@
+package problem.baekjoon.math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class No18310 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        System.out.println(arr[n % 2 == 0 ? (n >> 1) - 1 : (n >> 1)]);
+    }
+
+}
+
