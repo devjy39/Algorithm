43783b61dcf8b4c8aa1b6713b5ff0bd1f7c58514
jaeyeol <49369306+devjy39@users.noreply.github.com>
2023-07-06 23:01:55 +0900

    행복유치원 -그리디
    차이값을 정렬해서 차이가 적은순으로 집합 개수가 n개에서 k개가 될때까지 빼준다
    
    
    tc : O(nlogn)

---
+++ src/problem/baekjoon/greedy/No13164.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] heights = new int[n - 1];
+
+        st = new StringTokenizer(br.readLine());
+        int prev = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n - 1; i++) {
+            int number = Integer.parseInt(st.nextToken());
+
+            heights[i] = number - prev;
+            prev = number;
+        }
+
+        Arrays.sort(heights);
+
+        int price = 0;
+        for (int i = 0; i < n-k; i++) {
+            price += heights[i];
+        }
+
+        System.out.println(price);
+    }
+}
+
