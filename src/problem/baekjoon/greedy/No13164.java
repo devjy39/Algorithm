02a292fed85519b754d510a862c264714817fa3c
devjy39 <devjy0309@gmail.com>
2023-07-08 13:25:17 +0900

    modify: 클래스명 수정

--- src/problem/baekjoon/greedy/No13164.java
+++ src/problem/baekjoon/greedy/No13164.java
@@ -1,10 +1,12 @@
+package problem.baekjoon.greedy;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class No13164 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
