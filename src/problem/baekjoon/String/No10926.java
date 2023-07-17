dd724a6a640952c982f29a6f27509e76ac9b29ef
devjy39 <devjy0309@gmail.com>
2023-07-17 21:21:34 +0900

    수정

--- src/problem/baekjoon/String/No10926.java
+++ src/problem/baekjoon/String/No10926.java
@@ -1,8 +1,10 @@
+package problem.baekjoon.String;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class No10926 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
