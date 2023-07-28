42825b4db2e2fac5c21d155fb62a2d69c7c072aa
jaeyeol <49369306+devjy39@users.noreply.github.com>
2023-07-28 21:55:39 +0900

    a + b -2 - 구현
    pass

---
+++ src/problem/baekjoon/etc/No2558.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class No2558 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        
+        System.out.println(a+b);
+    }
+   
+}
+
