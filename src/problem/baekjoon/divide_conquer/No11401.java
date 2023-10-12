a7be4ae2dda89c53bccadc233b60c04333c5cc88
jaeyeol <49369306+devjy39@users.noreply.github.com>
2023-10-12 23:36:13 +0900

    이항 계수 3 - 분할정복
    페르마 소정리로 식을 구한 후 거듭제곱은 분할 정복을 통해 구한다.

---
+++ src/problem/baekjoon/divide_conquer/No11401.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class No11401 {
+    static final long MOD = 1_000_000_007;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        long nominator = factorial(n);
+        long denominator = (factorial(k) * factorial(n - k)) % MOD;
+
+        if (k == 0) {
+            System.out.println(1);
+        } else {
+            System.out.println((nominator * pow(denominator, MOD - 2)) % MOD);
+        }
+    }
+
+    private static long pow(long number, long exp) {
+        if (exp == 1) {
+            return number;
+        }
+
+        long halfResult = pow(number, exp >> 1);
+        long result = (halfResult * halfResult) % MOD;
+
+        if (exp % 2 == 1) {
+            return (result * number) % MOD;
+        }
+
+        return result;
+    }
+
+    private static long factorial(int n) {
+        long result = 1L;
+
+        while (n > 1) {
+            result = (result * n--) % MOD;
+        }
+
+        return result;
+    }
+
+}
